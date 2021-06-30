053a41ed64faa48e270e2f3004f5c04a29a5cfde
sumitpatil108 <69338434+sumitpatil108@users.noreply.github.com>
2021-06-30 16:03:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
-}
+var app = angular.module("myapp",[]);
+var res = document.getElementById('result');
+app.controller('control',function($scope){
+$scope.title = "Simple Interest Calculator";
+$scope.inputData = {};
+$scope.calculate = function(){
+  x = $scope.inputData;
+  amount = Number(x.amount);
+  rate = Number(x.rate);
+  time = Number(x.time);
+  ans = amount*time*rate/1200;
+  if(!isNaN(ans))
+	res.innerHTML = "The Simple Interest for the given Principal amount is " + parseInt(ans) + "/- rupees only";
+};
+});
         
